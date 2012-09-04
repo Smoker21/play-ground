d6ba84ca2bcc947f465a8646a4b9da6170c6675c
smoker21 <chen.lance@gmail.com>
2012-09-05 07:33:24 +0800

    init commit

---
+++ play-ground/src/main/java/com/rainsoft/play/App.java
@@ -0,0 +1,51 @@
+package com.rainsoft.play;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mvel2.MVEL;
+import org.mvel2.compiler.ExecutableStatement;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+
+		String express = "age > 40";
+		Map<String, Number> vars = new HashMap<String, Number>();
+		vars.put("age", 60);
+		MVEL.eval(express, vars);
+		System.out.println(MVEL.eval(express, vars));
+		Object o = MVEL.compileExpression(express);
+		Type[] types = o.getClass().getInterfaces();
+		for (int i = 0; i < types.length; i++) {
+			System.out.println(types[i]);
+		}
+		ExecutableStatement stmt = (ExecutableStatement) o;
+		HashMap a = new HashMap();
+		String script = "var colors = {'red', 'green', 'blue'}; "
+				+ "var a = 'Who cares'; " + " foreach (c : colors) { "
+				+ "     System.out.println(c + '!'); " + "}";
+		// In this case variable colors and a will store in hashMap a
+
+		System.out.println(MVEL.eval(script, a));
+		// In this case MVEL return a HashMap s
+		Object s = MVEL.eval(
+				"{\"Company\":'Rainty',\"What\":'Soft',\"name\":'Test'}", a);
+		System.out.println(s);
+		Object result = MVEL.eval("age > 40;", new HashMap() {
+			{
+				put("age", 43);
+			}
+		});
+		System.out.println(result);
+	}
+	
+	static class Person {
+		
+	}
+}
---
+++ play-ground/src/main/java/com/rainsoft/play/SimpleRule.java
@@ -0,0 +1,54 @@
+package com.rainsoft.play;
+
+import java.util.Map;
+
+public class SimpleRule {
+	private String ruleName; 
+	private String ruleCondition; 
+	private Map context;
+	public String getRuleName() {
+		return ruleName;
+	}
+	public void setRuleName(String ruleName) {
+		this.ruleName = ruleName;
+	}
+	public String getRuleCondition() {
+		return ruleCondition;
+	}
+	public void setRuleCondition(String ruleCondition) {
+		this.ruleCondition = ruleCondition;
+	}
+	public Map getContext() {
+		return context;
+	}
+	public void setContext(Map context) {
+		this.context = context;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((ruleName == null) ? 0 : ruleName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SimpleRule other = (SimpleRule) obj;
+		if (ruleName == null) {
+			if (other.ruleName != null)
+				return false;
+		} else if (!ruleName.equals(other.ruleName))
+			return false;
+		return true;
+	} 
+	
+	
+}
